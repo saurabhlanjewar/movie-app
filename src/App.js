6c9ad63a897da403ac8fc868461a979f4774a7ec
saurabh lanjewar <67968280+saurabhlanjewar@users.noreply.github.com>
2022-03-09 20:56:56 +0530

    Update App.js
    Changed api url from http to https

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   const [searchValue, setSearchValue] = useState('');
 
   const getMovieRequest = async (searchValue) => {
-    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=764058f1`;
+    const url = `https://www.omdbapi.com/?s=${searchValue}&apikey=764058f1`;
 
     const response = await fetch(url);
     const responseJson = await response.json();
